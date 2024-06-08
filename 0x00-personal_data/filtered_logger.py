5ecb8ee082d3f15af70804ad2ef46703a2c0f159
BelalGhonaim <belalpop1999@yahoo.com>
2024-06-08 16:48:50 +0300

    updated pycode

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -89,8 +89,8 @@ class RedactingFormatter(logging.Formatter):
     def format(self, record: logging.LogRecord) -> str:
         """Filters values from the log record
         """
-        formattedMessage = super(RedactingFormatter, self).format(record)
-        txt = filter_datum(self.fields, self.REDACTION, formattedMessage, self.SEPARATOR)
+        format_Mesge = super(RedactingFormatter, self).format(record)
+        txt = filter_datum(self.fields, self.REDACTION, format_Mesge, self.SEPARATOR)
         return txt
 
 
